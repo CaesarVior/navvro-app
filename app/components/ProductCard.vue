e2565807111dd3c70f487791692448304d1d4b04
Caesar Vior <viorcaesar@gmail.com>
2025-11-13 10:24:07 +0700

    Fixing conflict routing #!

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -37,7 +37,7 @@
                        text-green-600 border border-green-600 hover:bg-green-600 hover:text-white">
           Masukkan ke Keranjang
         </button>
-        <NuxtLink to="product/detail" class="w-full flex text-center justify-center py-2 rounded-lg text-sm font-medium transition-colors
+        <NuxtLink to="/product/detail" class="w-full flex text-center justify-center py-2 rounded-lg text-sm font-medium transition-colors
                 bg-green-700 text-white hover:bg-green-800">Lihat Produk</NuxtLink>
       </div>
     </div>
