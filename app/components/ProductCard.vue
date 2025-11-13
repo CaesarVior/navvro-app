affe58200948d47bb37ddf25e42c2c2ca36a4456
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-13 08:25:02 +0700

    Update ProductCard.vue

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -37,10 +37,8 @@
                        text-green-600 border border-green-600 hover:bg-green-600 hover:text-white">
           Masukkan ke Keranjang
         </button>
-        <button class="w-full py-2 rounded-lg text-sm font-medium transition-colors
-                       bg-green-700 text-white hover:bg-green-800">
-          Lihat Produk
-        </button>
+        <NuxtLink to="product/detail" class="w-full flex text-center justify-center py-2 rounded-lg text-sm font-medium transition-colors
+                bg-green-700 text-white hover:bg-green-800">Lihat Produk</NuxtLink>
       </div>
     </div>
   </div>
