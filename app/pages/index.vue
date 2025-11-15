7b0b4128efe1964de74ac9937a210fc9f2ecae4c
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-11-15 07:00:58 +0700

    slicing the swap

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,57 +8,20 @@
   <TheAbout />
   <CategoryCard />
   <ProductCollection />
-  
+  <TheSwap/>
+  <TheFooter/>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue'
 import CategoryFilter from '~/components/CategoryFilter.vue'
-import MsmeCard from '~/components/MsmeCard.vue'
-import Pagination from '~/components/Pagination.vue'
 import SearchBar from '~/components/SearchBar.vue'
 import TheAbout from '~/components/TheAbout.vue'
 import TheHeader from '~/components/TheHeader.vue'
-import msmes from '~/data/msme.json'
 
-const allMsmes = ref(msmes);
 const searchQuery = ref("");
 const categoryFilter = ref("");
 const currentPage = ref(1);
-const itemsPerPage = 6;
-
-const categories = computed(() => {
-  const allCategories = allMsmes.value.map((msme) => msme.category);
-  return ["Semua", ...new Set(allCategories)];
-});
-
-const filteredMsmes = computed(() => {
-  let result = allMsmes.value;
-
-  if (searchQuery.value) {
-    result = result.filter((msme) =>
-      msme.name.toLowerCase().includes(searchQuery.value.toLowerCase())
-    );
-  }
-
-
-
-  if (categoryFilter.value && categoryFilter.value !== "Semua") {
-    result = result.filter((msme) => msme.category === categoryFilter.value);
-  }
-
-  return result;
-});
-
-const totalPages = computed(() => {
-  return Math.ceil(filteredMsmes.value.length / itemsPerPage);
-});
-
-const paginatedMsmes = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  return filteredMsmes.value.slice(start, end);
-});
 
 const updateSearchQuery = (query) => {
   searchQuery.value = query;
